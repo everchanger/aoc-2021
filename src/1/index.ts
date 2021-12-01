10af36293c0c475f2fe3fd9098c97bea94e134a2
Joakim Rosenstam <joakim.rosenstam@gmail.com>
2021-12-01 08:35:21 +0100

    add solution for day 1

--- src/1/index.ts
+++ src/1/index.ts
@@ -1,64 +1,36 @@
-async function taskA (input: string): Promise<any> {
-  const sum = 2020
-  const inputArray = input.split('\n')
-
-  // Create a lookup table where the key is the sum - entry to quickly lookup the matching value
-  const lookup = new Map<number, number>()
-  for (const inputEntry of inputArray) {
-    const entryInt = parseInt(inputEntry)
-    const index = sum - entryInt
-    lookup.set(index, entryInt)
-  }
-
-  let result = 0
-  for (const needle of lookup.values()) {
-    const match = lookup.get(needle)
-    if (match !== undefined) {
-      result = match * needle
-      break
-    }
-  }
-
-  return result
+async function taskA (input: string): Promise<number> {
+  let count = 0
+  input.split('\n').reduce((previous: number | undefined, line) => {
+    count += +(previous < +line)
+    return +line
+  }, undefined)
+  return count
 }
 
-function findIndicesOfSum (sum: number, input: number[], usedIndices: number[]): number[] {
-  for (let i = 0; i < input.length; ++i) {
-    if (usedIndices.includes(i)) {
-      // We don't want to re-add an already run index!
-      continue
-    }
-    const entry = input[i]
-    const rest = sum - entry
-    if (rest > 0) {
-      // Add the current index to the usedIndex array and run this function again
-      const allreadyUsedIndices = [...usedIndices, i]
-      const result = findIndicesOfSum(rest, input, allreadyUsedIndices)
-      if (result.length > 0) {
-        return result
+async function taskB (input: string): Promise<number> {
+  const windowSize = 3
+  const frames = []
+
+  let count = 0
+  let frameOffset = 0
+  for (const line of input.split('\n')) {
+    frames.push({
+      count: 0,
+      value: 0,
+    })
+    for (let i = frameOffset; i < frames.length; ++i) {
+      const frame = frames[i]
+      frame.value += +line
+      frame.count++
+
+      if (frame.count === windowSize) {
+        count += +(frameOffset && frame.value > frames[i - 1].value)
+        frameOffset++
       }
-    } else if (rest === 0) {
-      // Nothing more to do, return the indices
-      return [...usedIndices, i]
-    }
-  }
-  // Looks like this is a dead end
-  return []
-}
-
-async function taskB (input: string): Promise<any> {
-  const sum = 2020
-  const inputArray = input.split('\n')
-  const numbers = inputArray.map(str => parseInt(str))
-
-  const indices = findIndicesOfSum(sum, numbers, [])
-
-  let result = 0
-  for (const index of indices) {
-    result = result === 0 ? numbers[index] : result * numbers[index]
+    }   
   }
 
-  return result
+  return count
 }
 
 export { taskA, taskB }
